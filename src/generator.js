db0ba809573a7b24066dc40c28691cd8946b7f96
ugo-reg <ugochi209@gmail.com>
2025-05-22 19:33:51 +0100

    improved my user experience

--- src/generator.js
+++ src/generator.js
@@ -12,15 +12,19 @@ function food(response) {
 
 function cook(event){
  event.preventDefault();
-
+ let instructionInput = document.querySelector("#user-instruction");
  let recipe=document.querySelector("#recipe");
- recipe.innerHTML= "cooking your recipe🍜🥢...."
+ recipe.innerHTML= `cooking your recipe🍜🥢 for ${instructionInput.value}....`;
+ let show=document.querySelector(".hidden");
+ show.classList.remove("hidden");
 
-let instructionInput=document.querySelector("#user-instruction");
+
 let apiKey = "3ee9046f22cb8dtcf3aa949o097a3347";
 let context = " user instructions are: you are a great chef, who is widely travelled and love to cook different cuisines from various countries, your mission is to generate easy to make cuisines in easy to understand words and basic HTML. make sure you follow the users instructions. sign the recipe with `Enjoy your meal with 💕 Ugochi😉`inside a <strong> center aligned remove the word `html` written on top ";
 let prompt = `generate short simple recipe about ${instructionInput.value}`;
 let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
+
+
 
 axios.get(apiUrl).then(food);
 
